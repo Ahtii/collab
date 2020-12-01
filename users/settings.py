b473aed27795ac36286d970880a7ca5b0b55a263
ScarEleven <11khanars@gmail.com>
2020-12-01 21:27:41 +0530

    integration of login and register with backend

--- users/settings.py
+++ users/settings.py
@@ -9,6 +9,6 @@ TOKEN_URL = "/api/auth/token"
 
 # google login
 CLIENT_ID = "973829616666-n71ceelkr8spfb1ldtt6318e54v1cebr.apps.googleusercontent.com"
-CLIENT_SECRETS_JSON = "/home/ahtisham/intern/collab/users/client_secret_973829616666-n71ceelkr8spfb1ldtt6318e54v1cebr.apps.googleusercontent.com.json"
+CLIENT_SECRETS_JSON = "/home/scareleven/collab/users/client_secret_973829616666-n71ceelkr8spfb1ldtt6318e54v1cebr.apps.googleusercontent.com.json"
 
 
